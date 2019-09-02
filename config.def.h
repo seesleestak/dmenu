df1f15e129ef76c0c5c8298f3acaf6bbd5828d78
Calvin Cieslak <calvin.cieslak@gmail.com>
2019-09-02 07:32:24 -0700

    Darken background, reinstate Meslo

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ECEFF4", "#2E3440" },
+	[SchemeNorm] = { "#ECEFF4", "#252A33" },
 	[SchemeSel] = { "#8FBCBB", "#3B4252" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
